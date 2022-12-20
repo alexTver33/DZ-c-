fe3840d7a86ca6f33a0b17764dddb5f792fd809d
Alexande <79607025659@yandex.ru>
2022-12-20 13:41:01 +0300

    2

---
+++ task2/Program.cs
@@ -0,0 +1,28 @@
+﻿int number1 = Convert.ToInt32 (Console.ReadLine());
+int number2 = Convert.ToInt32 (Console.ReadLine());
+int number3 = Convert.ToInt32 (Console.ReadLine());
+
+
+
+if (number1 > number2)
+
+   {
+         if (number1 > number3)
+            {
+                 Console.WriteLine("max:" + number1);
+            }
+
+        else
+        {
+                 Console.WriteLine("max:" + number3);
+        }
+}
+    
+else if (number2 > number3)
+        {
+             Console.WriteLine("max:" + number2);
+        }
+    else 
+    {
+         Console.WriteLine("max:" + number3);
+    }
